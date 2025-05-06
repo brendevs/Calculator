f7cd6c2c1b51dc840c95b39fc218d0bf631c0a96
jan brendell amandy <fbrendellcali@gmail.com>
2025-05-06 23:25:39 +0800

    add error

--- jav.js
+++ jav.js
@@ -1,27 +1,25 @@
 
-let operators = ['+', '-', '*', '/', '%'];
+const operators = ['+', '-', '*', '/', '%'];
 
 const display = document.getElementById('display');
 
 
-function
-    number (input) { 
-        display.value += input
-    } 
-function
-    bura (input) { 
-        display.value = ""
-    }
-function
-    operator (input) {
-        display.value += input 
-    }
-function
-    result (input) {
-        display.value = eval(display.value)
-    }
-function
-    divide (input) {
-        display.value += input
-    }
+function number (input) { 
+  display.value += input;
+} 
+function bura (input) { 
+      display.value = "";
+}
+function operator (input) {
+  
+  display.value += input
+}
+function result (input) {
+  try{
+    display.value = eval(display.value)
+  }
+  catch(error) {
+    display.value = "Error";
+  }
+}
 
