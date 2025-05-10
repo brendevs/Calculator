ed16827eb9d7c6d1139a52dab30ab03e2c7193bd
jan brendell amandy <fbrendellcali@gmail.com>
2025-05-10 21:23:38 +0800

    add erase function

--- jav.js
+++ jav.js
@@ -1,25 +1,23 @@
+const operators = ["+", "-", "*", "/", "%"];
 
-const operators = ['+', '-', '*', '/', '%'];
+const display = document.getElementById("display");
 
-const display = document.getElementById('display');
-
-
-function number (input) { 
+function number(input) {
   display.value += input;
-} 
-function bura (input) { 
-      display.value = "";
 }
-function operator (input) {
-  
-  display.value += input
+function bura(input) {
+  display.value = "";
 }
-function result (input) {
-  try{
-    display.value = eval(display.value)
-  }
-  catch(error) {
+function operator(input) {
+  display.value += input;
+}
+function result(input) {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
     display.value = "Error";
   }
 }
-
+function pahi() {
+  display.value = display.value.slice(0, -1);
+}
